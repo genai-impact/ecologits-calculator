403fb43ba8d9b28d0a5f7cb40d0d8aa99c23770c
Samuel Rince <s@rince.me>
2025-10-27 19:09:20 +0100

    fix: remove gemma models

--- src/constants.py
+++ src/constants.py
@@ -52,11 +52,7 @@ main_models_google = [
     "gemini-2.0-flash",
     "gemini-2.5-flash-lite",
     "gemini-2.5-flash",
-    "gemini-2.5-pro",
-    "gemma-3-1b-it",
-    "gemma-3-4b-it",
-    "gemma-3-12b-it",
-    "gemma-3-27b-it"
+    "gemini-2.5-pro"
 ]
 
 main_models_mistral = [
